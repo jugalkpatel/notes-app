63ea09420d693ad1157c992f6edaf41a13d7c50f
jimmy <ptjimmy99@gmail.com>
2022-02-09 11:52:10 +0530

    add Link Component for Page Navigation(static page navigation)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
 export default () => {
-  return <h1>index route</h1>;
+  return (
+    <>
+      <h1>index route</h1>
+      <Link href="/notes">to notes</Link>
+    </>
+  );
 };
